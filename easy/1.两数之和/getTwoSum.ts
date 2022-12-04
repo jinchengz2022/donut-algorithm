02059a4afd6f0ab7fed7311039d9830fbc924c2f
!!false <jinchengz202011@163.com>
2022-12-04 16:58:50 +0800

    feat: 两数之和

---
+++ easy/1.两数之和/getTwoSum.ts
@@ -0,0 +1,36 @@
+// 2022.12.04
+// brute force暴力解法
+export const getTwoSum1 = (nums: number[], target: number): number[] => {
+  if (nums.length === 0) return nums;
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = i + 1; j <= nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+}
+
+// 排序+双指针
+export const getTwoSum2 = (nums: number[], target: number): number[] => {
+  if (nums.length === 0) return nums;
+  // 先从小到大排序
+  // const sortNums = nums.sort((a, b) => a - b);
+  // 头尾指针
+  let start = 0, end = nums.length - 1;
+  while (start < end) {
+    if (nums[start] + nums[end] === target) {
+      return [start, end];
+    }
+    if (nums[start] + nums[end] < target) {
+      start++;
+    }
+    if (nums[start] + nums[end] > target) {
+      end--;
+    }
+  }
+  return [];
+}
+
+console.log(getTwoSum2([2, 5, 9, 37, 0], 9));
